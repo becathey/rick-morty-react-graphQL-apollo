896eb0764fb4b6fb9faf8254a9bb2b71033900ef
Bill Cathey <billcathey@hotmail.com>
2022-09-14 23:05:46 -0400

    Render characters, add styling

--- src/pages/CharactersList.jsx
+++ src/pages/CharactersList.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useQuery, gql } from "@apollo/client"
+import './CharactersList.css'
 
 const GET_CHARACTERS = gql`
     query {
@@ -16,9 +17,22 @@ const GET_CHARACTERS = gql`
 export default function CharactersList() {
     const {loading, data, error} = useQuery(GET_CHARACTERS)
     console.log({loading, data, error})
+    if (loading) return <h1>Loading ...</h1>
+    if (error) return <h1>Oops! Something went wrong.</h1>
 
     return (
-        <h1>Characters List</h1>
-        
+        <>
+            <h1>Rick and Morty Character List</h1>
+            <div className="character-list">
+                {data.characters.results.map(character => {
+                    return (
+                        <div className="character">
+                            <img src={character.image} className="character-image" />
+                            <h2 className="character-name">{character.name}</h2>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
     )
 }
